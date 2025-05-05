e5bc1932a4b254f7c54033704cf3d2a5cac0b1c9
xiaming.chen <chenxm35@gmail.com>
2025-05-05 22:26:23 +0800

    fix pydantic warning

--- tarsier/element.py
+++ tarsier/element.py
@@ -5,6 +5,6 @@ class Element(BaseModel):
     category: str = ""
     text: str = ""
     text_html: str = ""
-    box: list[list[float]] = []
+    box: tuple[tuple[float]] = tuple()
     langs: list[str] = []
     page_number: int = 0
